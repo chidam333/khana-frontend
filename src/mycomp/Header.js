987540604eebf304c120bb4d58ab8c36df60c7e6
Chidam <86545749+chidam333@users.noreply.github.com>
2022-06-22 00:53:56 +0530

    Update Header.js

--- src/mycomp/Header.js
+++ src/mycomp/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
     return (
         <nav className="navbar navbar-expand-lg ">
         <div className="container-fluid">
-          <Link className="navbar-brand" to='/searchl'>KHANA</Link>
+          <Link className="navbar-brand" to='/'>KHANA</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
